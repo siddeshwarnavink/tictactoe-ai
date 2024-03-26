7a2fa4175297c05e377cfa06f51f934933ec7463
Siddeshwar <siddeshwar.work@gmail.com>
2024-03-26 18:10:40 +0530

    added test cases for alpha beta pruning

--- src/test/kotlin/GameLogicTest.kt
+++ src/test/kotlin/GameLogicTest.kt
@@ -4,6 +4,7 @@ import kotlin.test.assertEquals
 import com.sidapps.tictactoeai.data.GameBoard
 import com.sidapps.tictactoeai.data.CellState
 import com.sidapps.tictactoeai.GameLogic
+import com.sidapps.tictactoeai.data.TreeNode
 
 class GameLogicTest {
     @Test
@@ -47,5 +48,33 @@ class GameLogicTest {
         ))
         assertEquals(false, GameLogic.checkWinner(nonWinningGameBoard, CellState.X))
         assertEquals(false, GameLogic.checkWinner(nonWinningGameBoard, CellState.O))
+    }
+
+    @Test
+    fun testMinimax() {
+        val rootNode = TreeNode(GameBoard())
+        val child1 = TreeNode(GameBoard()).apply { heuristicValue = 5 }
+        val child2 = TreeNode(GameBoard()).apply { heuristicValue = 3 }
+        val child3 = TreeNode(GameBoard()).apply { heuristicValue = 8 }
+        rootNode.addChild(child1)
+        rootNode.addChild(child2)
+        rootNode.addChild(child3)
+
+        val result = GameLogic.minimax(rootNode, 2, Int.MIN_VALUE, Int.MAX_VALUE, true)
+        assertEquals(8, result)
+    }
+
+    @Test
+    fun testAlphaBetaPruning() {
+        val rootNode = TreeNode(GameBoard())
+        val child1 = TreeNode(GameBoard()).apply { heuristicValue = 5 }
+        val child2 = TreeNode(GameBoard()).apply { heuristicValue = 3 }
+        val child3 = TreeNode(GameBoard()).apply { heuristicValue = 8 }
+        rootNode.addChild(child1)
+        rootNode.addChild(child2)
+        rootNode.addChild(child3)
+
+        val resultNode = GameLogic.alphaBetaPruning(rootNode, 2)
+        assertEquals(8, resultNode.heuristicValue)
     }
 }
