69185ed6fa6d357ea98552d0e5615c68f5215472
Siddeshwar <siddeshwar.work@gmail.com>
2024-03-29 09:52:49 +0530

    block resize and maximize window

--- src/main/kotlin/com/sidapps/tictactoeai/Main.kt
+++ src/main/kotlin/com/sidapps/tictactoeai/Main.kt
@@ -1,10 +1,16 @@
 package com.sidapps.tictactoeai.com.sidapps.tictactoeai
 
 import com.sidapps.tictactoeai.view.GameView
+import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
 
-class MyApp: App(GameView::class)
+class MyApp: App(GameView::class) {
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.resizableProperty().set(false)
+    }
+}
 
 fun main() {
     launch<MyApp>()
